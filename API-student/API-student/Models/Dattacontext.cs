29bec08abeb5d2181787b60efda90fc53d0622b6
Alejandro Sivila <a2019116316@estudiantes.upsa.edu.bo>
2020-09-08 13:11:50 -0400

    ya esta el studentscontroller

---
+++ API-student/API-student/Models/Dattacontext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace API_student.Models
+{
+    public class Dattacontext : DbContext
+    {
+        public Dattacontext(): base("DefaultConnection") //constructor
+        {
+        }
+
+        public System.Data.Entity.DbSet<API_student.Models.Student> Students { get; set; }
+    }
+}
